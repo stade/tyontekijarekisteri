94d210d0301842d89af02668daa8a147f3136813
tatu.tahvanainen <tatu.tahvanainen@32a7c819-599b-ba4d-e88a-2a25b288e8c0>
2010-09-29 10:25:10 +0000

    Luotu projekti.

---
+++ trunk/main.c
@@ -0,0 +1,22 @@
+/* 
+ * File:   main.c
+ * Author: tatutahv
+ *
+ * Created on 29. syyskuuta 2010, 13:15
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    int i;
+
+    i = 0;
+
+    return (EXIT_SUCCESS);
+}
+
