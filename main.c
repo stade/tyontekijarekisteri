a5ccf09387988f881cc16104696490483278847c
archmando <archmando@32a7c819-599b-ba4d-e88a-2a25b288e8c0>
2010-10-05 15:38:27 +0000

    Lisätty Linkitettylista moduuli ja testimaini sille (KAIKKI VITUN PAHASTI KESKEN RAGE)

--- main.c
+++ main.c
@@ -7,53 +7,59 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "hajautus.h"
+#include "linkitettylista.h"
 /*
  *
  */
 
 
  // TODO charit dynaamisiksi.
-struct tyontekija {
+typedef struct tyontekija {
      char etunimi[20];
      char sukunimi[30];
      int palkka;
      int aloitusvuosi;
      // TODO struct tyontekija seuraava;
 
-};
+}tyontekija;
 
-int lueTiedostosta();
+int lueTiedostosta(FILE*);
+int kirjoitaTiedostoon(struct tyontekija[], FILE*);
 int main(int argc, char** argv) {
 
-
-    lueTiedostosta();
-    return (EXIT_SUCCESS);
-}
-
-int lueTiedostosta() {
-
-    FILE *tiedosto;
-    struct tyontekija taulu[20]; //taulukollinen työntekijöitä, jotta meillä pointterit niihin (ennen hajautustaulun implementaatiota)
-    int j=0; //apumuuttuja taulun läpikäyntiin
+    FILE* tiedosto;
     char tiedostonimi[100];
 
     printf("Anna luettavan tiedoston nimi\n");
 
-
-    // Luetaan tiedostonimi käyttäjän syötteestä raijoitetaan luettavien
-    // merkkien määrä, jottei tule muistinylivuotoa.
     if (scanf("%99s", tiedostonimi) != 1) {
        fprintf(stderr, "Tiedostonimen lukeminen käyttäjältä epäonnistui!\n");
        return (EXIT_FAILURE);
     }
 
-    if ((tiedosto = fopen(tiedostonimi, "r" )) == NULL){ //TODO muuta fiksummaksi, nyt muuttuu käyttäjän mukaan
+    if ((tiedosto = fopen(tiedostonimi, "r" )) == NULL){
         fprintf(stderr, "Tiedoston avaaminen epäonnistu\n");
         return (EXIT_FAILURE);
     }
+    
+    lueTiedostosta(tiedosto); // <------- LUE
+    
+    if(fclose(tiedosto) == EOF) {
+        fprintf(stderr, "Tiedoston sulkeminen epäonnistui");
+        return EXIT_FAILURE;
+    }
 
+    kirjoitaTiedostoon(hajautustaulu, tiedosto); // <---- KIRJOITA
 
+    return (EXIT_SUCCESS);
+}
+
+int lueTiedostosta(FILE* tiedosto) {
+
+    struct tyontekija taulu[20]; //taulukollinen työntekijöitä, jotta meillä pointterit niihin (ennen hajautustaulun implementaatiota)
+    int j=0; //apumuuttuja taulun läpikäyntiin
+    
     while (feof(tiedosto) == 0) {
 
         struct tyontekija temp;
@@ -72,12 +78,15 @@ int lueTiedostosta() {
         j++;
     }
 
-    if(fclose(tiedosto) == EOF) {
-        fprintf(stderr, "Tiedoston sulkeminen epäonnistui");
-        return EXIT_FAILURE;
-
-    }
 
 
     return EXIT_SUCCESS;
+}
+int kirjoitaTiedostoon(struct tyontekija hajautustaulu[], FILE* tiedosto) {
+    for (int i=0; i<100; i++)
+        if (hajautustaulu[i]!=NULL)
+            fprintf(tiedosto, "%s %s %d %d", hajautustaulu[i].etunimi,
+                    hajautustaulu[i].sukunimi, hajautustaulu[i].aloitusvuosi,
+                    hajautustaulu[i].palkka);
+        else return(EXIT_FAILURE);
 }
