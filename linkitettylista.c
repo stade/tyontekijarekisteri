48d00dbc1843e7cc5044a165544beb83cb821551
tatu.tahvanainen <tatu.tahvanainen@32a7c819-599b-ba4d-e88a-2a25b288e8c0>
2010-10-06 11:43:13 +0000

    Toimiva testimain ja linkitettylista moduuli. Hienonnus kesken.

--- linkitettylista.c
+++ linkitettylista.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+
+
+typedef struct tyontekija {
+     char etunimi[20];
+     char sukunimi[30];
+     int palkka;
+     int aloitusvuosi;
+     // TODO struct tyontekija seuraava;
+
+}tyontekija;
+
 typedef struct lista{
     struct solmu *paa;
 
@@ -13,7 +25,7 @@ typedef struct solmu{
 lista* luoLista() {
      lista *p;
      p = ((lista *)malloc(sizeof(lista)));
-
+     p->paa=NULL;
      if (p == NULL) {
 
          return NULL;
@@ -38,58 +50,75 @@ solmu* luoSolmu(struct tyontekija *t) {
 
 
 }
-/*void lisaaListaanLoppuun(lista *listap, struct tyontekija *duunaaja) { //TODO työntekijöiden nimien vertaus
-    solmu* uusi = luoSolmu(duunaaja);
-    solmu* seuraava;
-    
-    if (listap.paa == NULL) {
-        lista.paa = uusi;
+int lisaaListaanLoppuun(lista *listap, struct tyontekija *duunaaja) { //TODO työntekijöiden nimien vertaus
+    struct solmu* uusi = luoSolmu(duunaaja);
+    struct solmu *seuraava;
+
+
+
+    if ((listap->paa) == NULL) {
+        listap->paa = uusi;
+        return 1;
     }
     else {
-        seuraava = lista.paa.seur;
-        while (seuraava != NULL) {
-            seuraava = seuraava.seur;
+        seuraava = listap->paa;
+
+        while ((seuraava->seur) != NULL) {
+            seuraava = seuraava->seur;
         }
-        seuraava.seur = uusi;
-        uusi.edell = seuraava;
+        seuraava->seur = uusi;
+        uusi->edell = seuraava;
+        return 2;
     }
+    return 0;
 }
-tyontekija* etsiListasta(lista l, char* etun, char* sukun) {
+
+tyontekija* etsiListasta(lista *l, char* etun, char* sukun) {
     if (l == NULL) {
         return NULL;
     }
-    solmu seuraava = l.paa;
-    while (seuraava != NULL) {
+    char *tempetu, *tempsuku;
 
-        if (strcmp(seuraava.duunari.etunimi, etun) == 0 &&
-            strcmp(seuraava.duunari.sukunimi, sukun) == 0) {
-            return seuraava.duunari;
-        } else seuraava = seuraava.seur;
+    printf("seuraavan alustus\n");
+    struct solmu* seuraava = l->paa;
+    while (seuraava != NULL) {
+        printf("etsinnän whilessa\n");
+        tempetu = seuraava->duunari->etunimi;
+        tempsuku = seuraava->duunari->sukunimi;
+        printf("%s\n", tempetu);
+        if (strcmp(tempetu, etun) == 0 &&
+            strcmp(tempsuku, sukun) == 0) {
+
+
+            printf("ETSITTY HOMO ON LÖYTYNYT\n");
+            return seuraava->duunari;
+        } else seuraava = seuraava->seur;
     }
     return NULL;
 }
-int poistaListasta(lista l, char* etun, char* sukun) { //return 0 if fail
-        if (l == NULL) {
-        return NULL;
+int poistaListasta(lista *l, char* etun, char* sukun) { //return 0 if fail
+    if (l == NULL) {
+        return 0;
     }
-    solmu seuraava = l.paa;
-    solmu poistettava;
+    solmu* seuraava = l->paa;
+    solmu* poistettava;
     while (seuraava != NULL) {
 
-        if (strcmp(seuraava.seur.duunari.etunimi, etun) == 0 &&
-            strcmp(seuraava.seur.duunari.sukunimi, sukun) == 0) {
-                poistettava = seuraava.seur;
-                seuraava.seur=poistettava.seur;
-                poistettava.seur=NULL;
-                vapautaSolmunVaraamaMuisti(poistettava);
+        if (strcmp(seuraava->seur->duunari->etunimi, etun) == 0 &&
+            strcmp(seuraava->seur->duunari->sukunimi, sukun) == 0) {
+                poistettava = seuraava->seur;
+                seuraava->seur=poistettava->seur;
+                poistettava->seur=NULL;
+                //vapautaSolmunVaraamaMuisti(poistettava);
                 return 1;
-        } else seuraava = seuraava.seur;
+        } else seuraava = seuraava->seur;
     }
     return 0;
 
 
 }
-void vapautaSolmunVaraamaMuisti(solmu s) {
+void vapautaSolmunVaraamaMuisti(solmu* s) {
+
     free(s);
     s=NULL;
-}*/
+}
--- testimain.c
+++ testimain.c
@@ -8,129 +8,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "linkitettylista.h"
 
 /*
  * 
  */
 
-typedef struct tyontekija {
-     char etunimi[20];
-     char sukunimi[30];
-     int palkka;
-     int aloitusvuosi;
-     // TODO struct tyontekija seuraava;
-
-}tyontekija;
-
-typedef struct lista{
-    struct solmu *paa;
-
-}lista;
-typedef struct solmu{
-    struct tyontekija *duunari;
-    struct solmu *seur;
-    struct solmu *edell;
-}solmu;
-lista* luoLista() {
-     lista *p;
-     p = ((lista *)malloc(sizeof(lista)));
-     p->paa=NULL;
-     if (p == NULL) {
-
-         return NULL;
-     }
-
-     return p;
-
-}
-solmu* luoSolmu(struct tyontekija *t) {
-     solmu *s;
-     s = (solmu *)malloc(sizeof(solmu));
-
-     if (s == NULL) {
-
-         return NULL;
-     }
-
-     s->duunari = t;
-     s->seur = NULL;
-     s->edell = NULL;
-     return s;
-
-
-}
-int lisaaListaanLoppuun(lista *listap, struct tyontekija *duunaaja) { //TODO työntekijöiden nimien vertaus
-    struct solmu* uusi = luoSolmu(duunaaja);
-    struct solmu *seuraava;
-    
-
-
-    if ((listap->paa) == NULL) {
-        listap->paa = uusi;
-        return 1;
-    }
-    else {
-        seuraava = listap->paa;
-        
-        while ((seuraava->seur) != NULL) {
-            seuraava = seuraava->seur;
-        }
-        seuraava->seur = uusi;
-        uusi->edell = seuraava;
-        return 2;
-    }
-    return 0;
-}
-
-tyontekija* etsiListasta(lista *l, char* etun, char* sukun) {
-    if (l == NULL) {
-        return NULL;
-    }
-    char *tempetu, *tempsuku;
-
-    printf("seuraavan alustus\n");
-    struct solmu* seuraava = l->paa;
-    while (seuraava != NULL) {
-        printf("etsinnän whilessa\n");
-        tempetu = seuraava->duunari->etunimi;
-        tempsuku = seuraava->duunari->sukunimi;
-        printf("%s\n", tempetu);
-        if (strcmp(tempetu, etun) == 0 &&
-            strcmp(tempsuku, sukun) == 0) {
- 
-
-            printf("ETSITTY HOMO ON LÖYTYNYT\n");
-            return seuraava->duunari;
-        } else seuraava = seuraava->seur;
-    }
-    return NULL;
-}
-int poistaListasta(lista *l, char* etun, char* sukun) { //return 0 if fail
-    if (l == NULL) {
-        return 0;
-    }
-    solmu* seuraava = l->paa;
-    solmu* poistettava;
-    while (seuraava != NULL) {
-
-        if (strcmp(seuraava->seur->duunari->etunimi, etun) == 0 &&
-            strcmp(seuraava->seur->duunari->sukunimi, sukun) == 0) {
-                poistettava = seuraava->seur;
-                seuraava->seur=poistettava->seur;
-                poistettava->seur=NULL;
-                //vapautaSolmunVaraamaMuisti(poistettava);
-                return 1;
-        } else seuraava = seuraava->seur;
-    }
-    return 0;
-
-
-}
-void vapautaSolmunVaraamaMuisti(solmu* s) {
-
-    free(s);
-    s=NULL;
-}
 
 int main(void) {
 
@@ -210,6 +93,7 @@ int main(void) {
      }
      printf("kolmas työntekijä:  %d\n", i);
 
+     printf("tyontetekijäkusee\n");
      struct tyontekija* testityontekija = malloc(sizeof(struct tyontekija));
      printf("ennen etsintää\n");
      testityontekija = etsiListasta(l, "matti", "kemppainen");
@@ -224,26 +108,16 @@ int main(void) {
 
 
      }
-     if ((testityontekija = etsiListasta(l, "jeesus", "poski")) == 0) {
 
-         printf("Poisto onnistui\n");
+      if ((testityontekija = etsiListasta(l, "jeesus", "poski")) == 0) {
 
-     }
+          printf("Poisto onnistui\n");
 
-
+      }
 
      free(t);
      free(k);
      free(l);
-
-
-     
-     
-
-
-
-
-
 
     return (EXIT_SUCCESS);
 }
