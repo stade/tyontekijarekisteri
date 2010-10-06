b56ad5b80a945f9089a66cd8eaac694ac595b860
tatu.tahvanainen <tatu.tahvanainen@32a7c819-599b-ba4d-e88a-2a25b288e8c0>
2010-10-06 11:43:13 +0000

    Toimiva testimain ja linkitettylista moduuli. Hienonnus kesken.

--- trunk/linkitettylista.h
+++ trunk/linkitettylista.h
@@ -1,6 +1,30 @@
-struct lista* luoLista();
-/*struct solmu* luoSolmu(struct tyontekija* t);
-void lisaaListaanLoppuun(lista* listap, tyontekija* duunaaja);
-tyontekija* etsiListasta(lista l, char* etun, char* sukun);
-int poistaListasta(lista l, char* etun, char* sukun);
-void vapautaSolmunVaraamaMuisti(solmu s);*/
+/*
+ linkitettylista.h
+ */
+
+typedef struct tyontekija {
+     char etunimi[20];
+     char sukunimi[30];
+     int palkka;
+     int aloitusvuosi;
+     // TODO struct tyontekija seuraava;
+
+}tyontekija;
+
+typedef struct lista{
+    struct solmu *paa;
+
+}lista;
+
+typedef struct solmu{
+    struct tyontekija *duunari;
+    struct solmu *seur;
+    struct solmu *edell;
+}solmu;
+
+lista* luoLista();
+solmu* luoSolmu(struct tyontekija *t);
+int lisaaListaanLoppuun(lista *listap, struct tyontekija *duunaaja);
+tyontekija* etsiListasta(lista *l, char* etun, char* sukun);
+int poistaListasta(lista *l, char* etun, char* sukun);
+void vapautaSolmunVaraamaMuisti(solmu* s);
